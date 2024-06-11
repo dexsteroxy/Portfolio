7ca4209fd1ec6962189d2dbc25cfd310924209a4
Amadi sixtus <dexsteroxy.1234@gmail.com>
2024-06-11 15:34:36 +0100

    creating a portfolio site using next.js three.js typescript .....

---
+++ components/Approach.tsx
@@ -0,0 +1,40 @@
+// import React from 'react'
+// import Image from 'next/image'
+// import Css from '../assets/tech/css.png'
+// import figma from '../assets/tech/figma.png'
+// import git from '../assets/tech/git.png'
+// import javasc from '../assets/tech/javascript.png'
+// import react from '../assets/tech/reactjs.png'
+// import tailwind from '../assets/tech/tailwind.png'
+// import redux from '../assets/tech/redux.png'
+// import html from '../assets/tech/html.png'
+// import typescript from '../assets/tech/typescript.png'
+// import three from '../assets/tech/threejs.svg'
+// import node from '../assets/tech/nodejs.png'
+// const Approach = () => {
+//   return (
+//     <div className=' py-20 w-full'>
+//          <h1 className="heading">
+//         My <span className="text-purple">work experience</span>
+//       </h1>
+// <div className=' '>
+// <div className=' h-28 w-28   justify-center items-center '>
+// <div><Image src={html} alt="css" /></div>
+// <div><Image src={Css} alt="" /></div>
+// <div><Image src={figma} alt="" /></div>
+// <div><Image src={git} alt="" /></div>
+// <div><Image src={javasc} alt="" /></div>
+// <div><Image src={ tailwind} alt="" /></div>
+// <div><Image src={ three} alt="" /></div>
+// <div><Image src={ typescript} alt="" /></div>
+// <div><Image src={node} alt="" /></div>
+// <div><Image src={react} alt="" /></div>
+// <div><Image src={ redux} alt="" /></div>
+// </div>
+// </div>
+
+//     </div>
+//   )
+// }
+
+// export default Approach
